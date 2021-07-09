26545d1d3aaa15d2e6bbb1e805ef9c08ea4d1236
Kyryl Shevchenko <dasoftweb@gmail.com>
2021-07-09 22:47:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const argv = require('yargs').argv;
+
+// const argv = require('yargs').argv;
+const { program } = require("commander");
 
 const {
   listContacts,
@@ -7,7 +9,16 @@ const {
   addContact,
 } = require("./contacts.js");
 
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 
+program.parse(process.argv);
+
+const argv = program.opts();
 
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
